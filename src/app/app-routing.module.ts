81ee45f461b9ed8e07da52ef714a31a02befb9b6
Yatin Chokshi <yatin.chokshi@imriel.com>
2023-01-30 22:13:48 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'work', component:WorkComponent},
+  {path:'about', component:AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
