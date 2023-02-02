231a6e50db017567b000cbb6fbcfa810a25884a5
Yatin1119 <yatinchokshi95@gmail.com>
2023-02-02 23:59:15 +0530

    made website responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,19 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
+ 
 })
 export class NavbarComponent {
   collapsed = true;
+  showNav = true;
+  hideNav = true;
   toggleCollapsed(): void {
     this.collapsed = !this.collapsed;
+    this.hideNav = this.collapsed;
+    this.showNav =!this.collapsed;
   }
 }
